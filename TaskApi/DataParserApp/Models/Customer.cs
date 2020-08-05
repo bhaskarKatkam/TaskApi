2ed677757f850dbca3afd42e999205933e9bfb62
bhaskarKatkam <bhaskar_kab@hotmail.com>
2020-08-05 20:28:25 +0530

    Create Customer.cs

---
+++ TaskApi/DataParserApp/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataParserApp.Models
+{
+    public partial class Customer
+    {
+        public int CustId { get; set; }
+        public string CustName { get; set; }
+        public int CustType { get; set; }
+        public decimal TotAmt { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
