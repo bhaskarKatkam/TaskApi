2588a3f692899706ce8687f688e8969914b7910b
bhaskarKatkam <bhaskar_kab@hotmail.com>
2020-08-05 20:28:29 +0530

    Create ResponseMessage.cs

---
+++ TaskApi/DataParserApp/Models/ResponseMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataParserApp.Models
+{
+    public class ResponseMessage
+    {
+        public string Message { set; get; }
+        public object Status { set; get; }
+        public object Data { set; get; }
+    }
+}
