d11e9b45afa9e7a45be37b48d0b4e3008b10f3bb
bhaskarKatkam <bhaskar_kab@hotmail.com>
2020-08-05 20:28:38 +0530

    Create TestDBContext.cs

---
+++ TaskApi/DataParserApp/Models/TestDBContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataParserApp.Models
+{
+    public partial class TestDBContext : DbContext
+    {
+        public TestDBContext()
+        {
+        }
+
+        public TestDBContext(DbContextOptions<TestDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=tcp:mydbtest1.database.windows.net,1433;Initial Catalog=TestDB;Persist Security Info=False;User ID=admin@123@mydbtest1;Password=Test@12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasKey(e => e.CustId)
+                    .HasName("PK__Customer__049E3AA9EDEC48BD");
+
+                entity.Property(e => e.CustId).ValueGeneratedNever();
+
+                entity.Property(e => e.CustName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.TimeStamp).HasColumnType("datetime");
+
+                entity.Property(e => e.TotAmt).HasColumnType("decimal(18, 2)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
