52a9bd9bd63b696d415ea969d2e0adf6237949ff
bhaskarKatkam <bhaskar_kab@hotmail.com>
2020-08-05 20:31:06 +0530

    Create IDataparser.cs

---
+++ TaskApi/DataParserApp/Repository/IDataparser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataParserApp.Models;
+
+namespace DataParserApp.Repository
+{
+    public interface IDataparser
+    {
+        bool SaveData(List<Customer> req);
+        List<Customer> GetDetails();
+    }
+}
